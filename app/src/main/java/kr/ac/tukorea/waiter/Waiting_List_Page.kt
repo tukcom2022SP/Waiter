5f991ba4cdab164a2ee66d3bf58deb5764e50804
ChuDongWon <ehddnjs1747@daum.net>
2022-07-26 21:07:08 +0900

    Waiting_List_Page First Commit

---
+++ app/src/main/java/kr/ac/tukorea/waiter/Waiting_List_Page.kt
@@ -0,0 +1,11 @@
+package kr.ac.tukorea.waiter
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Waiting_List_Page : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_waiting_list_page)
+    }
+}
