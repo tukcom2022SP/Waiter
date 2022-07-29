4f814e1f6aaa94923344f493d74591ca41dc68df
이경은 <saroojamin13@gmail.com>
2022-07-29 17:33:01 +0900

    add login page

---
+++ app/src/main/java/kr/ac/tukorea/waiter/LoginActivity.kt
@@ -0,0 +1,20 @@
+package kr.ac.tukorea.waiter
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+//import kr.ac.tukorea.kyungeun_login.databinding.ActivityLoginBinding
+import kr.ac.tukorea.waiter.databinding.ActivityLoginBinding
+
+class LoginActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityLoginBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.signinButton.setOnClickListener {
+            val intent = Intent(this,SignInActivity::class.java)
+        }
+    }
+}
