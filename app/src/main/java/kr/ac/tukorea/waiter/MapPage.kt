b11f9da44d963380f538c08bd471dead2fa547b9
Jungwook <nowrobin3@gmail.com>
2022-07-31 15:20:48 +0900

    Map marker trial 1

--- app/src/main/java/kr/ac/tukorea/waiter/MapPage.kt
+++ app/src/main/java/kr/ac/tukorea/waiter/MapPage.kt
@@ -4,16 +4,23 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.content.pm.PackageManager
 import android.Manifest;
+import android.annotation.SuppressLint
 import android.app.Instrumentation
+import android.location.Location
+import android.os.Looper
 import android.util.Log
 import android.webkit.PermissionRequest
 import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContract
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.annotation.NonNull
+import androidx.annotation.UiThread
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
+import com.google.android.gms.location.*
+import com.naver.maps.geometry.LatLng
 import com.naver.maps.map.*
+import com.naver.maps.map.overlay.Marker
 import java.util.jar.Pack200
 
 class MapPage : AppCompatActivity(), OnMapReadyCallback{
@@ -41,7 +48,10 @@ class MapPage : AppCompatActivity(), OnMapReadyCallback{
 //            else -> {}
 //        }
 //    }
-
+    val permission_request = 99
+    private lateinit var naverMap: NaverMap
+    lateinit var  fusedLocationProvideClient : FusedLocationProviderClient
+    lateinit var  locationCallback: LocationCallback
     var permissions = arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION, android.Manifest.permission.ACCESS_COARSE_LOCATION)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -53,14 +63,7 @@ class MapPage : AppCompatActivity(), OnMapReadyCallback{
 //        if (permission == PackageManager.PERMISSION_DENIED || permission1 == PackageManager.PERMISSION_DENIED)
 
 
-        fun startProcess(){
-            val fm  = supportFragmentManager
-            val mapFragment = fm.findFragmentById(R.id.map) as MapFragment?
-                ?: MapFragment.newInstance().also{
-                    fm.beginTransaction().add(R.id.map, it).commit()
-                }
-            mapFragment.getMapAsync(onMapReady)
-        }
+
         fun isPermitted(): Boolean {
             for (perm in permissions){
                 if (ContextCompat.checkSelfPermission(this, perm) != PackageManager.PERMISSION_GRANTED){
@@ -69,8 +72,14 @@ class MapPage : AppCompatActivity(), OnMapReadyCallback{
             }
             return true
         }
-
-
+        fun startProcess(){
+            val fm  = supportFragmentManager
+            val mapFragment = fm.findFragmentById(R.id.map) as MapFragment?
+                ?: MapFragment.newInstance().also{
+                    fm.beginTransaction().add(R.id.map, it).commit()
+                }
+            mapFragment.getMapAsync(this)
+        }
         if (isPermitted()){
             startProcess()
         }else{
@@ -81,12 +90,50 @@ class MapPage : AppCompatActivity(), OnMapReadyCallback{
             NaverMapSdk.NaverCloudPlatformClient("8eo4a3qdn1")
             return;
     }
-
-//    override fun onMapReady(naverMap: NaverMap) {
-//        val cameraPosition = CameraPosition(
-//        )
-//    }
+    @UiThread
+    override fun onMapReady(naverMap: NaverMap) {
+        val cameraPosition = CameraPosition(
+            LatLng(37.21312,126.321312),
+            16.0
+        )
+        naverMap.cameraPosition= cameraPosition
+        fusedLocationProvideClient =
+            LocationServices.getFusedLocationProviderClient(this)
+        setUpdateLocationListener()
+   }
+   @SuppressLint("MissingPermission")
+   fun setUpdateLocationListener() {
+       val locationRequest = LocationRequest.create()
+       locationRequest.run {
+           interval = 1000
+           priority = LocationRequest.PRIORITY_HIGH_ACCURACY
+       }
+       locationCallback = object  : LocationCallback(){
+           override fun onLocationResult(locationResult: LocationResult?) {
+           locationResult ?: return
+           for ((i, location) in locationResult.locations.withIndex()) {
+               Log.d("location: ", "${location.latitude},${location.longitude}")
+               setLastLocation(location)
+           }
+       }
+       }
+       fusedLocationProvideClient.requestLocationUpdates(
+           locationRequest,
+           locationCallback,
+           Looper.myLooper()
+       )
+   }
+    fun setLastLocation(location: Location) {
+        val myLocation =LatLng(location.latitude,location.longitude)
+        val marker = Marker()
+        marker.position = myLocation
+        marker.map = naverMap
+        val cameraUpdate = CameraUpdate.scrollTo(myLocation)
+        naverMap.moveCamera(cameraUpdate)
+   }
 }
+
+
 //    @Override
 //    public void onRequestPermissionsResult(var requestCode,String permissions[],int[] grandResults){
 //        if(grantResults.  )
