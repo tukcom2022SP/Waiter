6277da4a32db2787fd3f7041f4f2233b6a70c24f
Chu_dw <ehddnjs1747@daum.net>
2022-08-01 18:10:44 +0900

    add menu all page

--- app/src/main/java/kr/ac/tukorea/waiter/Waiting_List_Page.kt
+++ app/src/main/java/kr/ac/tukorea/waiter/Waiting_List_Page.kt
@@ -3,6 +3,7 @@ package kr.ac.tukorea.waiter
 import android.app.Activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
 import android.widget.ImageButton
 import android.widget.Toast
 import kr.ac.tukorea.waiter.databinding.ActivityWaitingListPageBinding
@@ -11,6 +12,13 @@ class Waiting_List_Page : AppCompatActivity() {
 
     private var Wbinding : ActivityWaitingListPageBinding? = null
     private val binding get() = Wbinding!!
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {  //메뉴
+        super.onCreateOptionsMenu(menu)
+        var mInflater = menuInflater
+        mInflater.inflate(R.menu.menu1,menu)
+        return true
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
