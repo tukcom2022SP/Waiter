3134b4e60702f8f751014bfb96c707ea8aadb0be
Chu_dw <ehddnjs1747@daum.net>
2022-08-08 02:09:20 +0900

    waiting List for owner DB

--- app/src/main/java/kr/ac/tukorea/waiter/Information_Registration_Page.kt
+++ app/src/main/java/kr/ac/tukorea/waiter/Information_Registration_Page.kt
@@ -5,6 +5,10 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
 import android.widget.Toast
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.storage.FirebaseStorage
 import kr.ac.tukorea.waiter.databinding.ActivityInformationRegistrationPageBinding
 
 class Information_Registration_Page : AppCompatActivity() {
@@ -25,6 +29,7 @@ class Information_Registration_Page : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
 
         mbinding = ActivityInformationRegistrationPageBinding.inflate(layoutInflater)
         setContentView(binding.root)
