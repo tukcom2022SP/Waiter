6ec93f97f10d50d0fcd21c100376d84630a17834
Jungwook <nowrobin3@gmail.com>
2022-08-08 14:11:37 +0900

    Search Bar Add

---
+++ app/src/main/java/kr/ac/tukorea/waiter/LinearLayout.kt
@@ -0,0 +1,8 @@
+package kr.ac.tukorea.waiter
+
+// 리사이클러 뷰 아이템 클래스
+class ListLayout(val name: String,      // 장소명
+                 val road: String,      // 도로명 주소
+                 val address: String,   // 지번 주소
+                 val x: Double,         // 경도(Longitude)
+                 val y: Double)         // 위도(Latitude)
